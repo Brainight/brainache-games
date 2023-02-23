964c042cbc6a1b19bd944b90f5e748a2fdfe8a0c
Brainight <lordstoneddog@gmail.com>
2023-02-23 23:32:53 +0100

    restructure and bwu

--- games.php
+++ games.php
@@ -13,7 +13,7 @@ use brainache\utils as bu;
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Brainache | Games</title>
-    <link rel="stylesheet" href="/css/generic.css">
+    <link rel="stylesheet" href="/bwu/css/generic.css">
     <link rel="stylesheet" href="/css/utils/header.css">
     <link rel="stylesheet" href="/css/games.css">
 </head>
